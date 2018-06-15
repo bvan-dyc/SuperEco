bc6e8178aacfd20e9200245ee236470c9e4074b6
bvan-dyc <33982019+bvan-dyc@users.noreply.github.com>
2018-06-15 22:48:34 +0200

    Project Init
    Project creation and simple movement script

---
+++ Assets/Scripts/Model/CharacterController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+	[SerializeField] [Range(0, 50)] private float	speed;
+	private Rigidbody2D rbody;
+
+	public void Awake()
+	{
+		rbody = GetComponent<Rigidbody2D>();
+	}
+
+	public void Move(Vector2 movement)
+	{
+		rbody.velocity = movement * speed;
+	}
+}
---
+++ Assets/Scripts/Utility/UserControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControl : MonoBehaviour
+{
+	CharacterController character;
+	bool enabledControl;
+
+	private void Awake()
+	{
+		character = GetComponent<CharacterController>();
+	}
+
+	private void FixedUpdate()
+	{
+		Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+		character.Move(movement);
+	}
+
+	public void enableControl()
+	{
+		enabledControl = true;
+	}
+
+	public void disableControl()
+	{
+		enabledControl = false;
+	}
+}
